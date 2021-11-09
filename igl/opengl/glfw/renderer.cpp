b99239011e4f8681066ba408585e25f27ef87d21
roye52371 <roi52371@gmail.com>
2021-11-09 19:51:41 +0200

    added rotate to object and scene, to check

--- igl/opengl/glfw/renderer.cpp
+++ igl/opengl/glfw/renderer.cpp
@@ -164,9 +164,8 @@ void Renderer::MouseProcessing(int button)
 		}
 		else
 		{
-			scn->data().MyRotate(scn->data().MakeTransd().block<3,3>(0,0) );
-
-			scn->data().MyRotate(scn->data().MakeTransd().block<3, 3>(0, 0));
+			scn->data().MyRotate(Eigen::Vector3d(1, 0, 0), yrel / 100.0);
+			scn->data().MyRotate(Eigen::Vector3d(0, 1, 0), xrel / 100.0);
 
 		}
 	}
@@ -186,8 +185,8 @@ void Renderer::MouseProcessing(int button)
 		}
 		else
 		{
-			scn->MyRotate(scn->MakeTransd().block<3, 3>(0, 0));
-			scn->MyRotate(scn->MakeTransd().block<3, 3>(0, 0));
+			scn->MyRotate(Eigen::Vector3d(1, 0, 0), yrel / 100.0);
+			scn->MyRotate(Eigen::Vector3d(0, 1, 0), xrel / 100.0);
 
 		}
 	}
