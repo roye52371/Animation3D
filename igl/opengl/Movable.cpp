6d8ff4d3a0a02d4302f49dfcb5e4b139e5e4b693
roye52371 <roi52371@gmail.com>
2021-11-16 11:00:14 +0200

    fixed rotate and moving

--- igl/opengl/Movable.cpp
+++ igl/opengl/Movable.cpp
@@ -14,7 +14,7 @@ Movable::Movable(const Movable& mov)
 
 Eigen::Matrix4f Movable::MakeTransScale()
 {
-	return (Tout.matrix()*Tin.matrix()).cast<float>();
+	return (Tout.matrix() * Tin.matrix()).cast<float>();
 }
 
 Eigen::Matrix4d Movable::MakeTransScaled()
@@ -32,8 +32,8 @@ Eigen::Matrix4d Movable::MakeTransd()
 
 void Movable::MyTranslate(Eigen::Vector3d amt, bool preRotation)
 {
-	
-	if(preRotation)
+
+	if (preRotation)
 		Tout.pretranslate(amt);
 	else
 		Tout.translate(amt);
@@ -54,6 +54,24 @@ void Movable::MyScale(Eigen::Vector3d amt)
 	Tin.scale(amt);
 }
 
+void Movable::SetCenterOfRotation(Eigen::Vector3d amt)
+{
+	Tout.pretranslate(amt);
+	Tin.pretranslate(-amt);
+
+}
+
+void Movable::MyTranslateInSystem(Eigen::Matrix3d rot, Eigen::Vector3d amt)
+{
+	Tout.pretranslate(rot.transpose() * amt);
+}
+
+void Movable::RotateInSystem(Eigen::Vector3d rotAxis, double angle)
+{
+	Tout.rotate(Eigen::AngleAxisd(angle, Tout.rotation().transpose() *
+		rotAxis.normalized())); //we will multiply the vector with the rotate matrix
+
+}
 
 
 
@@ -79,27 +97,3 @@ void Movable::MyScale(Eigen::Vector3d amt)
 
 
 
-
-//void Movable::TranslateInSystem(Eigen::Matrix4d Mat, Eigen::Vector3d amt, bool preRotation)
-//{
-//	Eigen::Vector3d v = Mat.transpose().block<3, 3>(0, 0) * amt; //transpose instead of inverse
-//	MyTranslate(v, preRotation);
-//}
-//
-//void Movable::RotateInSystem(Eigen::Matrix4d Mat, Eigen::Vector3d rotAxis, double angle)
-//{
-//	Eigen::Vector3d v = Mat.transpose().block<3, 3>(0, 0) * rotAxis; //transpose instead of inverse
-//	MyRotate(v.normalized(), angle);
-//}
-//
-//
-//void Movable::SetCenterOfRotation(Eigen::Vector3d amt)
-//{
-//	Tout.pretranslate(Tout.rotation().matrix().block<3, 3>(0, 0) * amt);
-//	Tin.translate(-amt);
-//}
-//
-//Eigen::Vector3d Movable::GetCenterOfRotation()
-//{
-//	return Tin.translation();
-//}
